beefb2e0a1b4b2a65d3d08dc15ffa2c6219c427b
dafinanz <112502897+dafinanz@users.noreply.github.com>
2022-08-31 21:44:46 +0700

    Add files via upload

---
+++ HelloWorld.java
@@ -0,0 +1,10 @@
+package Praktikum1;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("halo, ini source code OOP Java buatanku");
+	}
+
+}
